2a158bbe9ea9c9471d67eb572332e251f909cc12
TamasPinter <119883462+TamasPinter@users.noreply.github.com>
2023-01-25 20:24:49 -0500

    created most of README

--- README.md
+++ README.md
@@ -1,2 +1,58 @@
-# DayPlanner
-a composition for a daily planner
+# Easy Work-Day Planner
+
+## Description
+
+
+This is a simple easy to use planner for your daily activities. The app offers a great and easy to use UI with the ability to store information locally. This allows the user to enter data, close the tab and when they return later they can open the app and still see what they entered earlier for the day. Before coming into this project I had some difficulties operating the local storage and retrieval method, but while developing the code here I gained a greater understaning of that function. As well a better understaing of using the Dayjs function.
+
+## Table of Contents 
+
+
+- [Installation](#installation)
+- [Usage](#usage)
+- [Credits](#credits)
+- [License](#license)
+
+## Installation
+
+There is no installation required. Simply open the webpage in your browser.
+
+## Usage
+
+Once opening the webpage, the user can see all hours of their scheduled workday. Through this, they can see the current time as well at the top of the page. Looking down the calendar, any hours that have passed for the day will be shown in gray. the current hour in red, and any future hours available in green. 
+
+The user may click on any listed time, enter text in the time slot, and then click the save button on the side. Siply close the browser and open to the same page to see what you had entered before, retrieved from local storage.
+
+To delete any entries, simply highlight your text, erase it and save the field as blank.
+
+    
+    ![alt text](assets/images/screenshot.png)
+    
+
+## Credits
+
+UToronto Bootcamp for the lessons and modules which taught the functions to create this working project
+
+## License
+
+MIT License
+
+Copyright (c) 2023 TamasPinter
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
--- assets/script.js
+++ assets/script.js
@@ -2,7 +2,7 @@
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
 $(function () {
-    let timeNow = dayjs().format('ddd MMM-D-YYY H:m');
+    let timeNow = dayjs().format('MM-DD-YYYY');
     $("#currentDay").text(timeNow);
 
     // using the save button, info tracked through hour div and text input div, logged to localstorage
@@ -14,49 +14,30 @@ $(".saveBtn").on('click', function() {
     return;
 });
  
-//check if entered event is in past/present/future and assign variable colours
+//check to display timeblocks in correct colour on calendar tabs based on current time
 const thisHour = dayjs().format('H');
 $(".time-block").each(function () {
-    let divId = $(this).attr('id'); // divId tied to entered hour id
-    if (divId < thisHour) {  //check to make sure current iD is before
-        $(this).addClass('past'); //assign current frame past
-    } else if (divId > thisHour) {  //entered id greater than current time
-        $(this).addClass('future'); //assign class future
-    } else if (divId === thisHour) { //if Id is current hour
+    let divId = $(this).attr('id'); 
+    if (divId < thisHour) {  
+        $(this).addClass('past'); 
+    } else if (divId > thisHour) {  
+        $(this).addClass('future'); 
+    } else if (divId === thisHour) { 
         $(this).addClass('present');
     }
 });
 
 //grab local storage info
 
-$('9 .description').val(localStorage.getItem('9'));
-$('10 .description').val(localStorage.getItem('10'));
-$('11 .description').val(localStorage.getItem('11'));
-$('12 .description').val(localStorage.getItem('12'));
-$('13 .description').val(localStorage.getItem('13'));
-$('14 .description').val(localStorage.getItem('14'));
-$('15 .description').val(localStorage.getItem('15'));
-$('16 .description').val(localStorage.getItem('16'));
-$('17 .description').val(localStorage.getItem('17'));
+$('#9 .description').val(localStorage.getItem('9'));
+$('#10 .description').val(localStorage.getItem('10'));
+$('#11 .description').val(localStorage.getItem('11'));
+$('#12 .description').val(localStorage.getItem('12'));
+$('#13 .description').val(localStorage.getItem('13'));
+$('#14 .description').val(localStorage.getItem('14'));
+$('#15 .description').val(localStorage.getItem('15'));
+$('#16 .description').val(localStorage.getItem('16'));
+$('#17 .description').val(localStorage.getItem('17'));
 
-
-    // TODO: Add a listener for click events on the save button. This code should
-    // use the id in the containing time-block as a key to save the user input in
-    // local storage. HINT: What does `this` reference in the click listener
-    // function? How can DOM traversal be used to get the "hour-x" id of the
-    // time-block containing the button that was clicked? How might the id be
-    // useful when saving the description in local storage?
-    //
-    // TODO: Add code to apply the past, present, or future class to each time
-    // block by comparing the id to the current hour. HINTS: How can the id
-    // attribute of each time-block be used to conditionally add or remove the
-    // past, present, and future classes? How can Day.js be used to get the
-    // current hour in 24-hour time?
-    //
-    // TODO: Add code to get any user input that was saved in localStorage and set
-    // the values of the corresponding textarea elements. HINT: How can the id
-    // attribute of each time-block be used to do this?
-    //
-    // TODO: Add code to display the current date in the header of the page.
   });
   
